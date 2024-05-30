fa227ac592e0a77cbd162b99033f53d6ba8e4a27
Kulani Lakshika <lakkula@gmail.com>
2024-05-30 16:43:01 +0530

    feat: update grade

--- script.js
+++ script.js
@@ -8,6 +8,9 @@ function createMarkRow(){
     let markInput = document.createElement('input');
     markInput.placeholder = 'Mark';
     markInput.type = 'number';
+    markInput.addEventListener('change', function(){
+        updateGrade(this);
+    });
 
     let grade = document.createElement('p');
     grade.innerText = 'N/A';
@@ -16,4 +19,21 @@ function createMarkRow(){
 
     let markRowContainer = document.getElementById('mark-row-container');
     markRowContainer.appendChild(markRow);
+}
+
+function updateGrade(input){
+    let grade = input.parentElement.querySelector('p');
+    let marks = input.value;
+
+    if (marks >= 90){
+        grade.innerText = 'A';
+    }else if (marks >= 60){
+        grade.innerText = 'B';
+    }else if (marks >= 55){
+        grade.innerText = 'C';
+    }else if (marks >= 35){
+        grade.innerText = 'S';
+    }else{
+        grade.innerText = 'F';
+    }
 }
